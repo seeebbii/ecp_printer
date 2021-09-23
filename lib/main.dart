52aeabcb0148fc714dd3e53bdc2754a7928e32d9
haseeb zafar <Haseebzafar107@gmail.com>
2021-09-23 20:18:31 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:esc_pos_printer/esc_pos_printer.dart';
+import 'package:esc_pos_utils/esc_pos_utils.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+  void connect()async {
+    const PaperSize paper = PaperSize.mm80;
+    final profile = await CapabilityProfile.load();
+    final printer = NetworkPrinter(paper, profile);
+    final PosPrintResult res = await printer.connect('192.168.0.123', port: 9100);
+
+    print(res.msg) ;
+  }
+
+  @override
+  void initState() {
+    connect();
+    super.initState();
+  }
+
+
+  void testReceipt(NetworkPrinter printer) {
+    printer.text(
+        'Regular: aA bB cC dD eE fF gG hH iI jJ kK lL mM nN oO pP qQ rR sS tT uU vV wW xX yY zZ');
+    printer.text('Special 1: àÀ èÈ éÉ ûÛ üÜ çÇ ôÔ',
+        styles: PosStyles(codeTable: 'CP1252'));
+    printer.text('Special 2: blåbærgrød',
+        styles: PosStyles(codeTable: 'CP1252'));
+
+    printer.text('Bold text', styles: PosStyles(bold: true));
+    printer.text('Reverse text', styles: PosStyles(reverse: true));
+    printer.text('Underlined text',
+        styles: PosStyles(underline: true), linesAfter: 1);
+    printer.text('Align left', styles: PosStyles(align: PosAlign.left));
+    printer.text('Align center', styles: PosStyles(align: PosAlign.center));
+    printer.text('Align right',
+        styles: PosStyles(align: PosAlign.right), linesAfter: 1);
+
+    printer.text('Text size 200%',
+        styles: PosStyles(
+          height: PosTextSize.size2,
+          width: PosTextSize.size2,
+        ));
+
+    printer.feed(2);
+    printer.cut();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("ECP Printer"),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text('Print'),
+          ),
+        ));
+  }
+}
